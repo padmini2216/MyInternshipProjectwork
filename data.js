8eb5af53aebd6b6721846fe84a3d8f98e8adfbfd
padmini2216 <149585391+padmini2216@users.noreply.github.com>
2023-11-01 17:12:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,54 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'page.html/psy-trance-believer-ringtone-status-download-link-fs-creations-47336.mp3',
+        artist: 'artist 1',
+        cover: 'be.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'page.html/bts-fake-love-ringtone-44217.mp3',
+        artist: 'artist 2',
+        cover: 'playersss.jpg'
+    },
+    {
+        name: 'song 3',
+        path: 'page.html/stay-the-kid-laroi-justin-bieber-freeringtonesdownload-info-56767.mp3',
+        artist: 'artist 1',
+        cover: 'st.jpg'
+    },
+    {
+        name: 'song 4',
+        path: 'page.html/copines.mp3',
+        artist: 'artist 4',
+        cover: 'co.jpg'
+    },
+    {
+        name: 'song 5',
+        path: 'page.html/stay-the-kid-laroi-justin-bieber-freeringtonesdownload-info-56767.mp3',
+        artist: 'artist 5',
+        cover: 'st.jpg'
+    },
+    {
+        name: 'song 6',
+        path: 'page.html/copines.mp3',
+        artist: 'artist 6',
+        cover: 'co.jpg'
+    },
+    {
+        name: 'song 7',
+        path: 'page.html/psy-trance-believer-ringtone-status-download-link-fs-creations-47336.mp3',
+        artist: 'artist 7',
+        cover: 'be.jpg'
+    },
+    {
+        name: 'song 8',
+        path: 'page.html/bts-fake-love-ringtone-44217.mp3',
+        artist: 'artist 8',
+        cover: 'playersss.jpg'
+    },
+    
+    
+    
+]
+
